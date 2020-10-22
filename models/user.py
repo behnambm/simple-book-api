7039c96b4ac51a1b4910efbd7cd5c61ddd8b6f2d
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2020-10-22 18:21:02 +0330

    change in user model

--- models/user.py
+++ models/user.py
@@ -4,9 +4,10 @@ from werkzeug.security import generate_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(), nullable=False, unique=True)
+    first_name = db.Column(db.String(), nullable=False)
+    last_name = db.Column(db.String(), nullable=False)
     password_hash = db.Column(db.String(), nullable=False)
-    email = db.Column(db.String(), nullable=False)
 
     @property
     def password(self):
