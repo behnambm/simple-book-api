301cc847479af195fb39d53f302b2e9a6febf20d
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2020-10-22 18:35:34 +0330

    change populating user table inputs

--- manager.py
+++ manager.py
@@ -2,7 +2,7 @@ from flask_script import Manager
 from models import db
 from models.user import User
 from app import app
-from tqdm import trange
+from tqdm import tqdm
 
 manager = Manager(app)
 
@@ -11,12 +11,29 @@ manager = Manager(app)
 def init_db():
     db.create_all()
 
-    for i in trange(1, 4):
+    user_list = [
+        {
+            'first_name': 'Ben',
+            'last_name': 'blake',
+            'email': 'ben_blake@email.com'
+        },
+        {
+            'first_name': 'John',
+            'last_name': 'Smith',
+            'email': 'john_smith@email.com'
+        },
+        {
+            'first_name': 'hugo',
+            'last_name': 'alfred',
+            'email': 'hugo_alfred@email.com'
+        },
+    ]
+    for user in tqdm(user_list):
         'This is only for populating database.'
-
         tmp_user = User(
-            username='test_user_{}'.format(i),
-            email='test_mail_{}@email.com'.format(i),
+            first_name=user['first_name'],
+            last_name=user['last_name'],
+            email=user['email'],
             password='123'
         )
         tmp_user.save()
