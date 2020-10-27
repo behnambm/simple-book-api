b472be74c75f27172420c1c306f64e12396c1539
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2020-10-27 16:13:52 +0330

    add book model

--- models/user.py
+++ models/user.py
@@ -9,6 +9,7 @@ class User(db.Model):
     last_name = db.Column(db.String(), nullable=False)
     password_hash = db.Column(db.String(), nullable=False)
     roles = db.relationship('Role', secondary='user_roles')
+    books = db.relationship('Book', lazy='dynamic', backref='author')
 
     @property
     def password(self):
@@ -66,3 +67,19 @@ class UserRoles(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE')) # noqa
     role_id = db.Column(db.Integer, db.ForeignKey('role.id', ondelete='CASCADE')) # noqa
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    publish_date = db.Column(db.String, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+
+    @classmethod
+    def get_book_by_id(cls, book_id):
+        return cls.query.filter_by(id=book_id).first()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
